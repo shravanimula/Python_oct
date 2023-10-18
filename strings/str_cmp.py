c0bc98bfbe25ce14da88e2197ca3d8bd6b264545
shravani <shravani.mula@thundersoft.com>
2023-10-18 18:26:51 +0530

    string concept in python

---
+++ strings/str_cmp.py
@@ -0,0 +1,14 @@
+s1="iam working in c"
+s2="iam working in C"
+if s1 == s2:
+    print("strings are same")
+else:
+    print("strings are different")
+
+
+s1="iam working in python"
+s2="iam working in python"
+if s1 == s2:
+    print("strings are same")
+else:
+    print("strings are different")
